e10b2ee3ececdfdd9c3581a72f316ce7c77b5ae5
ilobes <127155323+ilobes@users.noreply.github.com>
2024-08-07 20:54:21 +0900

    1주차

---
+++ 1주차/5622.py
@@ -0,0 +1,23 @@
+count = 0
+word = input().upper()
+
+for char in word:
+    if char in 'ABC':
+        count += 3
+    elif char in 'DEF':
+        count += 4
+    elif char in 'GHI':
+        count += 5
+    elif char in 'JKL':
+        count += 6
+    elif char in 'MNO':
+        count += 7
+    elif char in 'PQRS':
+        count += 8
+    elif char in 'TUV':
+        count += 9
+    elif char in 'WXYZ':
+        count += 10
+
+print(count)
+
