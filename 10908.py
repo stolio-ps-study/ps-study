c3c90398a1429cd9dba39676add7db2e8e7107ac
parkseongeungit config --gobal user.name parkseongeun1112 <pse111204@gmail.com>
2024-08-14 07:01:29 +0900

    2주차

---
+++ 10908.py
@@ -0,0 +1,14 @@
+#10908
+#메모리 초과
+N = int(input())
+if N <= 100000000 :
+  nlist = [int(input()) for _ in range(N)]
+
+nlist.sort()
+
+
+
+for i in nlist:
+    print(i)
+
+
--- 1343.py
+++ 1343.py
@@ -1,44 +1,31 @@
+#1343
 # X가 홀수-> -1
-# .이 있을 떄 -> .을 기준으로 나눔 -> 홀수면 -1 짝수면 바꿈
-# 짝수일 떄-> X 개수를 4로 나눠서 몫이 1보다 크면 A -> 나머지: B
+# .이 있을 떄 -> .을 기준으로 나눔 -> 홀수면 -1 짝수면 바꿈 -> 합침
+# 짝수일 떄-> X 개수를 4로 나눠서 몫이 1보다 크면 A -> 나머지 2개 B
+#40분
+
 
 A=input()
+Xcount= A.count('X')
 
-Xcount=A.count('X')
-
-if Xcount % 2 == 0:
-  if '.' in A:
-    A_list=A.split('.')
-    for i in range(len(A_list)):
-      a=A_list[i]
-      if a.count('X')%2 !=0:
-        print(-1)
-        break
-      else:
-        a4=len(a)//4
-        if a4 >= 1:
-          a='AAAA'*a4+'B'*((len(a)%4)//2)
-        elif a == '':
-          continue
-        else:
-          a= a.replace(a,'BB')
-
-        A_list[i]=a
-
-    result = '.'.join(A_list)
-    print(result)
-
-
-  else:
-    A4=len(A)//4
-    if A4 >= 1:
-      A='AAAA'*A4+'B'*((len(A)%4)//2)
-    else:
-      A= 'BB'
-    print(A)
-
-else:
+if Xcount%2 != 0:
   print(-1)
+else:
+  if '.' in A:
+    A=A.split('.')
+    for i in range(len(A)):
+      if A[i].count('X') % 2 !=0:
+        print(-1) #종
+        exit()
+      elif A[i] == '':
+        continue
+      else:
+        A[i] = 'AAAA' * (len(A[i])//4) + 'BB' * ((len(A[i])%4//2))
+    result = '.'.join(A)
+  else:
+    result = 'AAAA' * (len(A)//4) + 'BB' * ((len(A)%4//2))
+
+  print(result)
 
 
 
